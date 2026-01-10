9c012902bfb0501ddcc3202b461398010beee963
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-10 18:46:54 +0600

    asdfghjklmnbvcxzqwertyuiop

---
+++ classTest/script.js
@@ -0,0 +1,26 @@
+function formSubmit(event){
+    event.preventDefault()
+    let name = document.getElementById("name").value;
+let contact = document.getElementById("contact").value;
+let remarks = document.getElementById("remarks").value;
+let location = document.getElementById("location").value;
+
+let sex = document.querySelector('input[name="sex"]:checked').value;
+let courses = document.querySelectorAll('input[name="courses"]:checked');
+
+let courseName = [];
+for(let i = 0 ; i< courses.length ; i++){
+    courseName.push(courses[i].value);
+}
+let output = "<br>  Name :" + name +
+             "<br>  Contact :" +  contact +
+             "<br>  Remarks :" + remarks  +
+             "<br>  Sex :" +  sex +
+             "<br>  Course Names :" + courseName   +
+             "<br>  Location :" + location
+
+let newWindow = window.open('', '_blank');
+newWindow.document.writeln(output);
+}
+let myForm = document.getElementById("myForm");
+myForm.addEventListener('submit', formSubmit);
