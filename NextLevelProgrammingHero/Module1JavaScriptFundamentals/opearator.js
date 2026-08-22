c1f9608916662503d6844dc87691cfcd68643aa5
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-08-22 22:38:50 +0600

    add

---
+++ NextLevelProgrammingHero/Module1JavaScriptFundamentals/opearator.js
@@ -0,0 +1,19 @@
+let a=10;
+let b= 40;
+
+let sum=a+b;
+
+console.log(sum);
+
+let division=a/b;
+
+console.log(division);
+
+console.log(typeof(division));
+
+let remainder=a%b;
+console.log(remainder);
+
+let d=a<b && 340;
+
+console.log(d);
---
+++ NextLevelProgrammingHero/Module1JavaScriptFundamentals/string.js
@@ -0,0 +1,21 @@
+let firstName="Mahbubur";
+
+let lastName="Rahman";
+
+let fullName=firstName+" "+lastName;
+
+console.log(fullName);
+
+fullName=`Md ${firstName} ${lastName} Sheikh \nLakshmipur`;
+
+console.log(fullName);
+
+let randomStr="I am a \" Student \"";
+
+console.log(randomStr);
+
+console.log(fullName.toUpperCase());
+
+console.log(fullName.includes("Laksh"));
+
+console.log(fullName.length);
