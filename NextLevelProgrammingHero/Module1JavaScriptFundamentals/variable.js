491690ea5b1fad341b48738a990f9ad92f684af9
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-08-22 21:16:34 +0600

    add

---
+++ NextLevelProgrammingHero/Module1JavaScriptFundamentals/variable.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+
+let name1="Mahbub";
+
+name1="Mahbub R Rahman";
+
+const age=40;
+
+let isOld=true;
+
+
+console.log(name1);
+
+console.log(age);
+
+console.log(typeof(name1));
+
+console.log(typeof(age));
+
+console.log(typeof(isOld));
