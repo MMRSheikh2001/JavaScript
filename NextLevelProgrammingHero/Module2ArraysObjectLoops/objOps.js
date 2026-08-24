4483b36c3ddbdac2e9e30f84e28e792215a51d3a
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-08-25 04:37:44 +0600

    add

---
+++ NextLevelProgrammingHero/Module2ArraysObjectLoops/objOps.js
@@ -0,0 +1,45 @@
+let student = {
+    name: "Rahim",
+    id: 102,
+    marks: {
+        bangla: 75,
+        english: 82,
+        math: 90,
+    },
+};
+
+let totalMarks = 0;
+let totalSubjects = 0;
+for (let ele in student.marks) {
+
+    totalMarks += student.marks[ele];
+    totalSubjects++;
+}
+
+console.log(totalMarks);
+console.log(totalSubjects);
+let avg = totalMarks / totalSubjects;
+console.log(totalMarks / totalSubjects);
+console.log(avg.toFixed(2));
+
+if (avg >= 80) {
+    console.log("Got A+");
+}
+
+
+// task: koto gulo item kinechen and koto tk bill hoyeche shese BDT te dekhate hobe print kore
+
+let cart = [
+    { name: "Shirt", price: 1200, quantity: 2 },
+    { name: "Pants", price: 1800, quantity: 1 },
+    { name: "Socks", price: 150, quantity: 3 },
+];
+
+let TotalItem = 0;
+let totalBill = 0;
+
+for (let items of cart) {
+
+
+
+}
